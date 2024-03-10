dd5ac63228969f6e7d03390c8db59f75270faa82
MatO <balu@abhis.com>
2024-03-10 20:05:32 +0530

    Sending Static data to db

---
+++ NodeApi-ProjectNotes.md
@@ -0,0 +1,3 @@
+# Node API
+
+> We use postman to send post request to the server instead of 'form' element.We can use postman to generate get put post or deleted requests.Istall postman from the web according to your os.
--- app.js
+++ app.js
@@ -12,23 +12,42 @@ const PORT = process.env.PORT;
 
 mongoose
   .connect(
-    "mongodb+srv://bmatomato:balumathew@cluster0.bkhkhc6.mongodb.net/bmdatabase?retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb+srv://bmatomato:balumathew@cluster0.bkhkhc6.mongodb.net/backendApi?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then(() => {
     console.log("DATABASE CONNECTED");
   })
   .catch((e) => console.log(e));
 
+const schema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+
+const User = mongoose.model("User", schema);
 app.get("/", (req, res) => {
   res.send("Nice working");
 });
 
 // We are going to make an api to access all users in the url localhost:/users/all
 
-app.get("/users/all", (req, res) => {
+app.get("/users/all", async (req, res) => {
+  const users = await User.find({});
   res.json({
     success: true,
-    users: [],
+    users,
+  });
+});
+app.post("/users/new", async (req, res) => {
+  await User.create({
+    name: "Arun",
+    email: "Arun@gamil.com",
+    password: "password",
+  });
+  res.json({
+    success: true,
+    message: "Registered Succesfully",
   });
 });
 
